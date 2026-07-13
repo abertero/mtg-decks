6b45ee203b7c02eab225f9731234a0ee2ac23214
abertero <thescientist14@gmail.com>
2026-07-13 11:16:52 -0400

    Adds ttd

--- ttd/test.py
+++ ttd/test.py
@@ -18,7 +18,9 @@ with open(file_path, 'r', encoding='utf-8') as f:
     text = f.read()
 
 pipeline = KPipeline(lang_code='e')
-voice = 'ef_dora'
+#voice = 'ef_dora'
+voice = 'em_santa'
+pause_duration_ms = 1000
 generator = pipeline(text, voice=voice, speed=1, split_pattern=r'\n+')
 
 all_audio = []
@@ -27,7 +29,17 @@ for i, (gs, ps, audio) in enumerate(generator):
     all_audio.append(audio)
 
 if all_audio:
-    combined = np.concatenate(all_audio)
+    sample_rate = 24000
+    silence_samples = int(sample_rate * pause_duration_ms / 1000)
+    silence = np.zeros(silence_samples, dtype=np.float32)
+    
+    audio_with_pauses = []
+    for i, audio in enumerate(all_audio):
+        audio_with_pauses.append(audio)
+        if i < len(all_audio) - 1:
+            audio_with_pauses.append(silence)
+    
+    combined = np.concatenate(audio_with_pauses)
     wav_path = 'voice\\output.wav'
     sf.write(wav_path, combined, 24000)
     display(Audio(data=combined, rate=24000, autoplay=True))
