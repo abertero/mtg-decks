9eee371cb452def7a6012972357714cf66a46711
abertero <thescientist14@gmail.com>
2026-07-21 20:49:17 -0400

    New chapter

--- tts/create_all.bat
+++ tts/create_all.bat
@@ -1,14 +1,16 @@
 python .\tts.py .\story\outro.txt --voice es-CL-CatalinaNeural
 
-python .\pipeline.py .\json\002-tarkir\01-the-madness-of-sarkhan.json
-python .\pipeline.py .\json\002-tarkir\02-awakening-the-bear.json
-python .\pipeline.py .\json\002-tarkir\03-sorins-revelation.json
-python .\pipeline.py .\json\002-tarkir\04-taigams-scheming.json
-python .\pipeline.py .\json\002-tarkir\05-way-of-the-mantis.json
-python .\pipeline.py .\json\002-tarkir\06-the-chensal-twins.json
-python .\pipeline.py .\json\002-tarkir\07-enlightened.json
-python .\pipeline.py .\json\002-tarkir\08-the-salt-road.json
-python .\pipeline.py .\json\002-tarkir\09-mercy.json
-python .\pipeline.py .\json\002-tarkir\10-victory.json
-python .\pipeline.py .\json\002-tarkir\11-bond-and-blood.json
-python .\pipeline.py .\json\002-tarkir\12-journey-nexus.json
+python .\pipeline.py .\json\002-khans-of-tarkir\01-the-madness-of-sarkhan.json
+python .\pipeline.py .\json\002-khans-of-tarkir\02-awakening-the-bear.json
+python .\pipeline.py .\json\002-khans-of-tarkir\03-sorins-revelation.json
+python .\pipeline.py .\json\002-khans-of-tarkir\04-taigams-scheming.json
+python .\pipeline.py .\json\002-khans-of-tarkir\05-way-of-the-mantis.json
+python .\pipeline.py .\json\002-khans-of-tarkir\06-the-chensal-twins.json
+python .\pipeline.py .\json\002-khans-of-tarkir\07-enlightened.json
+python .\pipeline.py .\json\002-khans-of-tarkir\08-the-salt-road.json
+python .\pipeline.py .\json\002-khans-of-tarkir\09-mercy.json
+python .\pipeline.py .\json\002-khans-of-tarkir\10-victory.json
+python .\pipeline.py .\json\002-khans-of-tarkir\11-bond-and-blood.json
+python .\pipeline.py .\json\002-khans-of-tarkir\12-journey-nexus.json
+
+python .\pipeline.py .\json\003-fate-reforged\01-new-tarkir-old.json
